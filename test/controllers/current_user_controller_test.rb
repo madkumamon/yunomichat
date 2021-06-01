ebec730bfdb9acd9e8540d3a1240c464b7fd2e83
Oleksii Kartamyshev <893147+yunomizen@users.noreply.github.com>
2021-06-01 12:26:40 +0300

    Add serializer, migration, controllers and configs

---
+++ test/controllers/current_user_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class CurrentUserControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get current_user_index_url
+    assert_response :success
+  end
+end
