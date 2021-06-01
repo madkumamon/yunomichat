4995b173353b30c25ca5a85e3e0f8b35e59bbb1c
Oleksii Kartamyshev <893147+yunomizen@users.noreply.github.com>
2021-06-01 13:57:35 +0300

    fix: correct username permit, correct controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  validates_uniqueness_of :username
+  # validates_uniqueness_of :username
 
   def jwt_payload
     super.merge("foo" => "bar")
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@
 
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at
+  attributes :id, :email, :created_at, :username
 
   attribute :created_date do |user|
     user.created_at && user.created_at.strftime("%d/%m/%Y")
